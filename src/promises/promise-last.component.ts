0268b554fbf1484e22fbdfadf2ae1975bf69cd67
BANULSKI Norbert <norbert.banulski@gmail.com>
2023-09-29 12:11:09 +0200

    cale zadanie z promisami

--- src/promises/promise-last.component.ts
+++ src/promises/promise-last.component.ts
@@ -60,7 +60,6 @@ export class PromiseLastComponent implements OnInit {
             })
             .catch(() => {
               resolve(lastCompletedPromise);
-              completedPromises++
             });
       });
     });
