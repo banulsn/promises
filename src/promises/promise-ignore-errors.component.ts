e5cc8fde83561db449fbddddfeac04ac2e9dd174
BANULSKI Norbert <norbert.banulski@gmail.com>
2023-10-04 14:23:34 +0200

    funkcje promiseIgnoreErrors

--- src/promises/promise-ignore-errors.component.ts
+++ src/promises/promise-ignore-errors.component.ts
@@ -22,8 +22,8 @@ export class PromiseIgnoreErrorsComponent implements OnInit {
         this.arrayOfPromises.push(promiseThatRejectsAfterNSecondsGenerator(0));
         this.arrayOfPromises.push(promiseThatRejectsAfterNSecondsGenerator(1));
         this.arrayOfPromises.push(promiseThatRejectsAfterNSecondsGenerator(2));
-        this.arrayOfPromises.push(promiseThatRejectsAfterNSecondsGenerator(3));
         this.arrayOfPromises.push(promiseThatResolvesAfterNSecondsGenerator(4));
+        this.arrayOfPromises.push(promiseThatRejectsAfterNSecondsGenerator(3));
         this.arrayOfPromises.push(promiseThatResolvesAfterNSecondsGenerator(2));
 
         this.promiseIgnoreErrorsRef = this.promiseIgnoreErrors(this.arrayOfPromises)
@@ -36,7 +36,7 @@ export class PromiseIgnoreErrorsComponent implements OnInit {
     }
 
     // PROMISE.IGNORE_ERRORS FUNCTION
-   promiseIgnoreErrors(promises) {
+    promiseIgnoreErrors(promises) {
         return new Promise((resolve, reject) => {
             if (!Array.isArray(promises)) {
                 return reject('promises muszą być tablicą');
@@ -52,49 +52,50 @@ export class PromiseIgnoreErrorsComponent implements OnInit {
             promises.forEach((promise) => {
                 Promise.resolve(promise)
                     .then((result) => {
-                        results.push(result);
                         completedPromises++;
+                        results.push(result);
 
                         if (completedPromises === promises.length) {
                             resolve(results);
                         }
                     })
-                    .catch((error) => {
+                    .catch(() => {
                         completedPromises++;
-                        console.log('Catch error in promiseIgnoreErrors: ', error)
+                        if (completedPromises === promises.length) {
+                            resolve(results);
+                        }
                     });
             });
         });
     }
 
     async promiseIgnoreErrorsUsingAsyncAwait(promises) {
-        return new Promise((resolve, reject) => {
-            // if (!Array.isArray(promises)) {
-            //     return reject('promises muszą być tablicą');
-            // }
-            //
-            // const results = [];
-            // let completedPromises = 0;
-            //
-            // if (!promises.length) {
-            //     resolve(results);
-            // }
-            //
-            // promises.forEach((promise) => {
-            //     Promise.resolve(promise)
-            //         .then((result) => {
-            //             results.push(result);
-            //             completedPromises++;
-            //
-            //             if (completedPromises === promises.length) {
-            //                 resolve(results);
-            //             }
-            //         })
-            //         .catch((error) => {
-            //             completedPromises++;
-            //             console.log('Catch error in promiseIgnoreErrors: ', error)
-            //         });
-            // });
+        if (!Array.isArray(promises)) {
+            return Promise.reject('promises muszą być tablicą');
+        }
+
+        const results = [];
+        let completedPromises = 0;
+
+        if (!promises.length) {
+            return Promise.resolve(results);
+        }
+
+        return new Promise((resolve) => {
+            promises.forEach(async (promise) => {
+                try {
+                    results.push(await Promise.resolve(promise));
+                    completedPromises++;
+                    if (completedPromises === promises.length) {
+                        resolve(results);
+                    }
+                } catch (error) {
+                    completedPromises++;
+                    if (completedPromises === promises.length) {
+                        resolve(results);
+                    }
+                }
+            })
         });
     }
 
